54e91d711ff0a54b31a88c2db5aed9d12d9799fa
Jorge Falcon <jfalcon95@outlook.com>
2015-10-11 09:30:55 -0500

    parse lost ppl

--- sources/js/map.js
+++ sources/js/map.js
@@ -11,3 +11,33 @@ $(document).ready(function(){
         $("#petForm")[0].reset();
     });
 });
+
+Parse.initialization("R6bSzRV4B4p0PPOonPBfJMYOmVzc30ekeJhyQUEv", "amWdFqFhwmP7jLGPh0zCI2ozm5pjVdoqz9wiTGeL");
+
+var ParseObj = Parse.Object.extend('PersonPost');
+
+$('#personForm').submit(function(e){
+    e.preventDefault();
+
+    var data = {
+        Name        : $("#personFirstName").val() + " " + $("#personLastName").val(),
+        Age         : $("#personAge").val(),
+        LastSeen    : $("#personLastSeen").val(),
+        Description : $("#personDescription").val(),
+        Illness     : $("#personIllness").val(),
+        Sex         : $("#personSex").val(), 
+    };
+
+    parseObj = new ParseObj();
+
+    parseObj.save(data, {
+        success     : function(parseObj){
+                        console.log(parseObj.get('Name') + " " + parseObj.get('Age'));
+        },
+        error       : function(parseObj, error){
+                        console.log(parseObj);
+                        console.log("Error: " + error.message);
+        }   
+
+    });
+});
