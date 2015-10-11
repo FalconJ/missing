eb56de6720496d6c633bc312922b6762b2ff9fea
Jorge Falcon <jfalcon95@outlook.com>
2015-10-11 00:23:28 -0500

    fix of fix

--- sources/js/parse.js
+++ sources/js/parse.js
@@ -1,4 +1,4 @@
-Parse.initialize("HNtPCmySZi4pjv9wn9k4B406hmWDHsxUSSoQDa1b", "ssQ2yXNF7fWN4aPX9XS2w2BkT5bNW7QHPt1vDCBU");
+Parse.initialize("R6bSzRV4B4p0PPOonPBfJMYOmVzc30ekeJhyQUEv", "amWdFqFhwmP7jLGPh0zCI2ozm5pjVdoqz9wiTGeL");
 
 function registerUser(fbinfo){
 
@@ -15,7 +15,7 @@ function registerUser(fbinfo){
 	user.set("username", fbinfo[2]);
 	user.set("email", fbinfo[2]);
 	user.set("sex", fbinfo[5]);
-	user.set("email", fbinfo[6]);
+	user.set("photo", fbinfo[6]);
 
 	user.signUp(null, {
 
@@ -24,7 +24,7 @@ function registerUser(fbinfo){
 			window.location = "login.html";
 			return true;
 		},
-		error: function(user, error){
+		error: function(error){
 			return false;			
 		}
 	});
@@ -39,7 +39,7 @@ function logUser(fbinfo){
         Parse.User.logOut();
     } 
 
-	Parse.User.logIn(fbinfo[1], fbinfo[0], {
+	Parse.User.logIn(fbinfo[2], fbinfo[0], {
 		success: function(){
 			console.log("Welcome back bro");
 	    	window.location = "login.html";
