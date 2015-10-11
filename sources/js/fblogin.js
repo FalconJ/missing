bf3f45229606e5c8aa47bc9949ad5548cc310953
Jorge Falcon <jfalcon95@outlook.com>
2015-10-10 22:39:30 -0500

    status false

--- sources/js/fblogin.js
+++ sources/js/fblogin.js
@@ -36,7 +36,8 @@
     cookie     : true,  // enable cookies to allow the server to access
                         // the session
     xfbml      : true,  // parse social plugins on this page
-    version    : 'v2.5' // use version 2.2
+    version    : 'v2.5', // use version 2.2
+    status     : false
   });
 
   // Now that we've initialized the JavaScript SDK, we call
